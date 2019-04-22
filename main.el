c70c1f7c6264a1a800e463353c4c182f0689ad67
mengkaer <carlmorris@jd.com>
2019-04-22 14:10:42 +0800

    Move org varaible sets to hook function.
    Move setting of org mode variables to a new hook function cm-org-hook.  In
    particular the variable org-latex-classes is bound in a lazily loaded org
    packaged ox-latex, so that variable cannot be modified until after that package
    is loaded. Using with-eval-after-load fixes that problem.

--- main.el
+++ main.el
@@ -1,29 +1,40 @@
+
+;;; Org Mode
 (require 'org-tempo)
-(add-hook 'org-mode-hook #'visual-line-mode)
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((emacs-lisp . t)
    (python . t)))
-(setq org-confirm-babel-evaluate nil)
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
 (setq python-shell-interpreter
       ;; Handle tilde expansion.
       (expand-file-name "~/AppData/Local/Continuum/anaconda2/python.exe"))
-(setq org-babel-python-command python-shell-interpreter)
+
 (setq python-shell-unbuffered nil)
+(defun cm-org-hook ()
+  ;; Add support for elegantnote LaTeX class.
+  (with-eval-after-load 'ox-latex
+    (add-to-list 'org-latex-classes
+                 '("elegantnote"
+                   "\\documentclass[en]{elegantnote}"
+                   ("\\section{%s}" . "\\section*{%s}")
+                   ("\\subsection{%s}" . "\\subsection*{%s}")
+                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                   ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                   ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+  (setq org-babel-python-command python-shell-interpreter)
+  (setq org-confirm-babel-evaluate nil)
+  )
+
+(add-hook 'org-mode-hook #'visual-line-mode)
+(add-hook 'org-mode-hook #'cm-org-hook)
+
+
+;;; Python Mode
 (add-hook 'python-mode-hook
           (lambda ()
             (add-to-list 'python-shell-completion-native-disabled-interpreters "python.exe")))
 
-;; Add support for elegantnote LaTeX class.
-(add-to-list 'org-latex-classes
-             '("elegantnote"
-               "\\documentclass[en]{elegantnote}"
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
 (load "abbrev.el")
